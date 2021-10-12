b9bd5fb6370df1e6a280f69a0185c57e4e683589
xuejixiaoyao <91312065+xuejixiaoyao@users.noreply.github.com>
2021-10-12 16:19:50 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,37 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	.container{
+		width: 100%;
+		height: calc(100vh - 80px);
+		overflow: hidden;
+	}
+	.container scroll-view{
+		height: 100%;
+	}
+	.fixbg{
+		width: 100%;
+		height: 100vh;
+		position: fixed;
+		top: 0;
+		left: 0;
+		background-size: cover;
+		background-position: center 0; 
+		/* 毛玻璃效果 */
+		filter: blur(10px);
+		transform:scale(1.2);
+		}
+</style>
